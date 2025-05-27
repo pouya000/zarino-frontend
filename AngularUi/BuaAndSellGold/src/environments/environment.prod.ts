8d61ffdba92bf887e9914d96eabf2f568525a4cb
pouya000 <puya.shamsi@gmail.com>
2025-05-27 19:08:21 +0330

    Added AngularUi folder to repository

---
+++ AngularUi/BuaAndSellGold/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+// export const environment = {
+//   production: true
+// };
+
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://zarino-backend.onrender.com/api'
+};
