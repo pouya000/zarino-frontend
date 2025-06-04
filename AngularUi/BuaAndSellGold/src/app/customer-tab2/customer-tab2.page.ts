c724c291d6f7acd5dd5d09d78634bf63006e2a96
pouya000 <puya.shamsi@gmail.com>
2025-06-04 19:18:11 +0330

    add customer-tab2.ts ionViewWillEnt

--- AngularUi/BuaAndSellGold/src/app/customer-tab2/customer-tab2.page.ts
+++ AngularUi/BuaAndSellGold/src/app/customer-tab2/customer-tab2.page.ts
@@ -276,6 +276,13 @@ export class CustomerTab2Page implements OnInit {
 
   }
 
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter - هر بار که وارد تب ۲ می‌شوی اجرا می‌شود');
+    this.wsService.getPriceUpdates().subscribe((newPrice) => {
+      console.log("receive price is: ", newPrice)
+      this.latestChangePrice()
+    });
+  }
 
   doRefresh(event: any) {
     console.log('Pull event detected. Refreshing...');
